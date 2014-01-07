ddc0776965b1fed5f66e9722ee9a231d45312586
hunter497 <hunter.497@gmail.com>
2014-01-07 08:48:25 -0500

    First commit. Setting up the evercraft kata @ codemash

---
+++ EvercraftKata/tests/CharacterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EvercraftKata.tests
+{
+    [TestFixture]
+    public sealed class CharacterTests
+    {
+        [Test]
+        public void GetName()
+        {
+            var character = new Character("Nick");
+            Assert.AreEqual("Nick", character.getName());
+        }
+    }
+}
